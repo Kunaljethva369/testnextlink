99a64f5ce7c94c916df73b6099545dee87a27115
Kunal jethva <kunaljethva90@gmail.com>
2023-06-27 12:35:55 +0530

    inital commit

---
+++ src/app/Components/Navbar.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <ul style={{display:'flex'}}>
+            <li style={{marginLeft:'20px',listStyle:'none'}}>
+                <Link href={'/'}>Home</Link>
+            </li>
+            <li style={{marginLeft:'20px',listStyle:'none'}}>
+                <Link href={'/about'}>About</Link>
+            </li>
+            <li style={{marginLeft:'20px',listStyle:'none'}}>
+                <Link href={'/service'}>Service</Link>
+            </li>
+            <li style={{marginLeft:'20px',listStyle:'none'}}>
+                <Link href={'/contact'}>Contact</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
