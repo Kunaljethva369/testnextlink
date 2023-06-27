9e35e4253a128560e0346971279ffe43ce3b6cb7
Kunal jethva <kunaljethva90@gmail.com>
2023-06-27 13:00:35 +0530

    remove bracket

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -6,16 +6,16 @@ function Navbar() {
     <div>
         <ul style={{display:'flex'}}>
             <li style={{marginLeft:'20px',listStyle:'none'}}>
-                <Link href={'/'}>Home</Link>
+                <Link href='/'>Home</Link>
             </li>
             <li style={{marginLeft:'20px',listStyle:'none'}}>
-                <Link href={'/about'}>About</Link>
+                <Link href='/about'>About</Link>
             </li>
             <li style={{marginLeft:'20px',listStyle:'none'}}>
-                <Link href={'/service'}>Service</Link>
+                <Link href='/service'>Service</Link>
             </li>
             <li style={{marginLeft:'20px',listStyle:'none'}}>
-                <Link href={'/contact'}>Contact</Link>
+                <Link href='/contact'>Contact</Link>
             </li>
         </ul>
     </div>
